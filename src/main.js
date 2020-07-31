4a0da61b197e2ef534b9e3bfef5b71db94e0437e
ahaow <ahao72@qq.com>
2020-07-31 17:42:22 +0800

    wh

--- src/main.js
+++ src/main.js
@@ -11,6 +11,17 @@ Vue.prototype.$http = { get, post }
 Vue.prototype.$loading = Loading;
 Vue.prototype.$Dialog = Dialog;
 
+// Vue.directory("click-outside", {
+//   bind(el, binding, vnode) {
+//     function documentHandler(e) {
+//       const _target = e._target;
+//       if(el.contains(_target)) return false;
+//       if(binding.expression) binding.value(e)
+//     }
+//   }
+// })
+
+
 new Vue({
   router,
   store,
